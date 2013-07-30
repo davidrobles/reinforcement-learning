171488a57951443820de40390d44cd5288a34efc
David Robles <drobles@gmail.com>
2013-07-30 10:58:02 -0700

    change package structure

--- src/dr/rl/policies/StochasticPolicy.java
+++
@@ -1,9 +0,0 @@
-package dr.rl.policies;
-
-interface StochasticPolicy<S, A>
-{
-    /**
-     * Returns the probability of taking the given action in the given state.
-     */
-    double getProbability(S state, A action);
-}
--- src/dr/rl/valuefunctions/QFunctionObserver.java
+++
@@ -1,6 +0,0 @@
-package dr.rl.valuefunctions;
-
-public interface QFunctionObserver<S, A>
-{
-    void qFunctionUpdated(QFunction<S, A> qFunction);
-}
